e87ab96ddfaa0ac0aeff37be52a28c814fac938f
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2020-06-04 02:38:26 +0700

    add cart-items and connect cart-item-dropdowns and show items cart with redux

--- src/components/cart-dropdown/cart-dropdown-component.jsx
+++ src/components/cart-dropdown/cart-dropdown-component.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
 import CustomButton from "../buttons/custom-button-component";
 import "./cart-dropdown-style.scss";
+import CartItems from "../cart-items/cart-item-component";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItems key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECK OUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
